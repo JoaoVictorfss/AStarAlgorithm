02d98d69e96406275a54e4bfd549f563fd9a9134
João Victor Fernandes <jvsfernandes924@gmail.com>
2022-06-01 21:39:41 -0300

    Adicionado metodo para comparar matrizes e comparar nos

--- src/main.py
+++ src/main.py
@@ -12,7 +12,10 @@ matriz_obj_2 = [[0,1,2],[3,4,5],[6,7,8]]
 matriz_teste = [[7,2,4],[5,0,6],[8,3,1]]
 
 teste = Node(matriz_teste, 1, 1)
+teste2 = Node(matriz_teste, 1, 1)
+
 borda.adicionar_no(teste)
+
 print(borda.total_de_nos())
 primeiro = borda.obter_primeiro_no()
 print(borda.total_de_nos())
--- src/models/Node.py
+++ src/models/Node.py
@@ -1,3 +1,4 @@
+import numpy as np
 class Node:
     def __init__(self, matriz, xb, yb):
         self.matriz = matriz
@@ -10,36 +11,55 @@ class Node:
         self.h = 0 
         self.f = self.g + self.h 
 
-def crie_matriz(n_linhas, n_colunas):
-    matriz = []
-    for i in range(n_linhas):
-        linha = []
-        linha += [input()]
-        for i in linha:
-            linha = i.split(',')
-        matriz += [linha]
-    return matriz
-    
-def __anda_esquerda(matriz_t, pos_1, pos_2):
-      temp = matriz_t[pos_1][pos_2]
-      matriz_t[pos_1][pos_2] = matriz_t[pos_1][pos_2-1]
-      matriz_t[pos_1][pos_2-1] = temp
-      return matriz_t
+    def __eq__(self, outro_no):
+        matriz1 = np.array(self.matriz)
+        matriz2 = np.array(outro_no.matriz)
+        
+        return (self.xb == outro_no.xb and 
+                  self.yb == outro_no.yb and 
+                  self.f == outro_no.f and 
+                  self.h == outro_no.h and 
+                  self.g == outro_no.g and
+                  self.mov_qtd == outro_no.mov_qtd and 
+                 (matriz1 == matriz2).all()
+        )
+        
+    def crie_matriz(n_linhas, n_colunas):
+        matriz = []
+        for i in range(n_linhas):
+            linha = []
+            linha += [input()]
+            for i in linha:
+                linha = i.split(',')
+            matriz += [linha]
+        return matriz
 
-def __anda_direita(matriz_t, pos_1, pos_2):
-      temp = matriz_t[pos_1][pos_2]
-      matriz_t[pos_1][pos_2] = matriz_t[pos_1][pos_2+1]
-      matriz_t[pos_1][pos_2+1] = temp
-      return matriz_t
+    def compara_matrizes(self, no):
+        matriz1 = np.array(self.matriz)
+        matriz2 = np.array(no.matriz)
+        return (matriz1 == matriz2).all()
+        
+    def __anda_esquerda(matriz_t, pos_1, pos_2):
+        temp = matriz_t[pos_1][pos_2]
+        matriz_t[pos_1][pos_2] = matriz_t[pos_1][pos_2-1]
+        matriz_t[pos_1][pos_2-1] = temp
+        return matriz_t
 
-def __anda_cima(matriz_t, pos_1, pos_2):
-      temp = matriz_t[pos_1][pos_2]
-      matriz_t[pos_1][pos_2] = matriz_t[pos_1-1][pos_2]
-      matriz_t[pos_1-1][pos_2] = temp
-      return matriz_t
+    def __anda_direita(matriz_t, pos_1, pos_2):
+        temp = matriz_t[pos_1][pos_2]
+        matriz_t[pos_1][pos_2] = matriz_t[pos_1][pos_2+1]
+        matriz_t[pos_1][pos_2+1] = temp
+        return matriz_t
 
-def __anda_baixo(matriz_t, pos_1, pos_2):
-      temp = matriz_t[pos_1][pos_2]
-      matriz_t[pos_1][pos_2] = matriz_t[pos_1+1][pos_2]
-      matriz_t[pos_1+1][pos_2] = temp
-      return matriz_t
+    def __anda_cima(matriz_t, pos_1, pos_2):
+        temp = matriz_t[pos_1][pos_2]
+        matriz_t[pos_1][pos_2] = matriz_t[pos_1-1][pos_2]
+        matriz_t[pos_1-1][pos_2] = temp
+        return matriz_t
+
+    def __anda_baixo(matriz_t, pos_1, pos_2):
+        temp = matriz_t[pos_1][pos_2]
+        matriz_t[pos_1][pos_2] = matriz_t[pos_1+1][pos_2]
+        matriz_t[pos_1+1][pos_2] = temp
+        return matriz_t
+        
